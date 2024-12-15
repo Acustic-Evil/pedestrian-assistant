a331103cea250dd4d3e15d384314c49cc98f5bb3
acustic evil <prisha931@gmail.com>
2024-12-15 14:35:23 +0300

    init

---
+++ src/main/java/com/pedestrianassistant/PedestrianAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.pedestrianassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedestrianAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PedestrianAssistantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pedestrianassistant/PedestrianAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedestrianassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedestrianAssistantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
